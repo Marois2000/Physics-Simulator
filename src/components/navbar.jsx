2cb490fd1494d2723b1bdb4480139ab35093bf37
Marois2000 <tm1207@plymouth.edu>
2023-11-30 11:21:03 -0500

    testing being done in about page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <div className="w-full justify-start items-center p-10 flex gap-[10rem]">
+        <div className="w-full justify-start items-center p-10 flex gap-[10rem] bg-black">
             <div>
                 <h1 className="text-white font-montserrat text-5xl font-bold">Physics</h1>
                 <h1 className="text-white font-montserrat text-5xl font-bold ml-10">Simulator</h1>
             </div>
 
-            <Link className="text-white text-2xl" to={"/"}>Home</Link>
-            <Link className="text-white text-2xl" to={"/about"}>About</Link>
-            <Link className="text-white text-2xl" to={"/"}>Contact</Link>
+            <Link className="text-white text-2xl font-montserrat" to={"/"}>Home</Link>
+            <Link className="text-white text-2xl font-montserrat" to={"/about"}>About</Link>
+            <Link className="text-white text-2xl font-montserrat" to={"/"}>Contact</Link>
 
 
 
