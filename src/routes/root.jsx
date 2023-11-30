f6a5d0bcc657676c5dd0817cceef86dfca716c6b
Marois2000 <tm1207@plymouth.edu>
2023-11-30 14:05:47 -0500

    commit

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,10 +5,19 @@ import { ProblemCard } from "../components/problemcard";
 
 export const Root = () => {
     return (
-        <div className="flex flex-col justify-start items-start h-full w-full bg-black">
+        <div className="flex flex-col justify-start items-center h-full w-full bg-black">
             <Navbar />
 
             
+            <div className="flex justify-between w-[80%] items center my-20">
+                <div className="flex flex-col justify-center">
+                    <h1 className="text-white font-montserrat text-3xl font-bold">Try a Problem</h1>
+                    <p className="text-white text-lg font-montserrat w-[40ch]">Click on one of the problems below and see if you can get it right!</p>
+                </div>
+            </div>
+
+
+
             <ProblemCard link="/seesaw" title="See Saw" desc="Can you figure out how the boxes will balance?"/>
 
         </div> 
