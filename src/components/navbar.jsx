74de203eb90bbfb1a0ae2646f8d97a3732d67b0d
Marois2000 <tm1207@plymouth.edu>
2023-12-01 21:21:57 -0500

    links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,9 +10,9 @@ export const Navbar = () => {
                 <h1 className="text-white font-montserrat text-5xl font-bold ml-10">Simulator</h1>
             </div>
 
-            <Link className="text-white text-2xl font-montserrat" to={"/"}>Home</Link>
+            <Link className="text-white text-2xl font-montserrat" to={""}>Home</Link>
             <Link className="text-white text-2xl font-montserrat" to={"/about"}>About</Link>
-            <Link className="text-white text-2xl font-montserrat" to={"/"}>Contact</Link>
+            <Link className="text-white text-2xl font-montserrat" to={""}>Contact</Link>
 
 
 
