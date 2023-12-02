6e4239471a09ee2063330727a254cc42fe8a02e1
Marois2000 <tm1207@plymouth.edu>
2023-12-01 21:13:33 -0500

    url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./Phyics-Simulator",
+  base: "https://marois2000.github.io/Physics-Simulator/",
   plugins: [react()],
 })
