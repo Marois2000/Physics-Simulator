09878d150af5f6d07a081ddf71e613e78be4a684
ReeceXB <reecexavier@gmail.com>
2023-12-05 15:42:21 -0500

    started random seesaw

--- src/routes/seesaw.jsx
+++ src/routes/seesaw.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Engine, Render, Bodies, World, Body, Matter, Constraint, Vector } from 'matter-js'
 import React from 'react'
 import { Navbar } from '../components/navbar'
@@ -8,11 +8,16 @@ export const SeeSaw = (props) => {
   const isPressed = useRef(false)
   const engine = useRef(Engine.create())
 
+  const [randomX, setRandomX] = useState();
+  const setMeasurements = () => {
     
-  useEffect(() => {
+  }
+
+    useEffect(() => {
         const cw = 800;
         const ch = 650;
 
+   
         addSeeSaw();
 
         const render = Render.create({
@@ -59,10 +64,14 @@ export const SeeSaw = (props) => {
     var catapult =  Bodies.rectangle(400, 520, 320, 20,{ collisionFilter: { group: group } });
     var rectangle1 = Bodies.rectangle(400, 600, 800, 50.5, { isStatic: true, render: { fillStyle: '#060a19' } });
     //var rectangle2 = Bodies.rectangle(250, 555, 20, 50, { isStatic: true, render: { fillStyle: '#060a19' } });
-    var rectangle3 = Bodies.rectangle(400, 535, 20, 80, { isStatic: true, collisionFilter: { group: group }, render: { fillStyle: '#060a19' } });
+    var rectangle2 = Bodies.rectangle(400, 535, 20, 80, { isStatic: true, collisionFilter: { group: group }, render: { fillStyle: '#060a19' } });
     
-    var weight1 = Bodies.rectangle(500, 480, 50,50);
-    var weight2 = Bodies.rectangle(300, 480, 50,50);
+    var weight1 = Bodies.rectangle(500, 485, 50,50,{ isStatic: true});
+    //var weight2 = Bodies.rectangle(300, 490, 50,50,{ isStatic: true});
+    let randomX2 = Math.random()*100+266;
+    setRandomX(randomX2);
+    var weight2 = Bodies.rectangle(randomX2, 485, 50,50,{ isStatic: false} );
+    // Random Range from 266-365 2.66cm-3.65cm 4cm is center
     
     //var circle1 = Bodies.circle(560, 100, 50, { density: 0.005 });
     var constraint =  Constraint.create({ 
@@ -71,9 +80,11 @@ export const SeeSaw = (props) => {
         stiffness: 1,
         length: 0
     });
-    World.add(engine.current.world, [weight1, weight2, constraint, catapult, group,rectangle1, rectangle3]);
+    World.add(engine.current.world, [weight1, weight2, constraint, catapult, group,rectangle1, rectangle2]);
     //World.add(engine.current.world, [boxA, boxB]);
   }
+
+  
 
   return (
     <div className='w-[100%] h-[100%] bg-black'>
